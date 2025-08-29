2e3fb60feee0e1313139be40769e267d051ac051
Taha <ta753090@gmail.com>
2025-08-28 21:49:34 -0400

    File added

---
+++ Conditionals/zy.cpp
@@ -0,0 +1,21 @@
+#include <iostream> 
+using namespace std;
+int main() {
+
+    int numPostcards;
+    cin >> numPostcards;
+    cout << numPostcards;
+   
+    cout << numPostcards << "\n" << "post ";
+
+    
+  
+   
+   
+   
+
+
+
+
+return 0;
+}
