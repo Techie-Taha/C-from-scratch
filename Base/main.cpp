ac2b61be38f823bfd09dafafe0017725f3370edf
Taha <ta753090@gmail.com>
2025-08-31 03:19:39 -0400

    File added

--- Base/main.cpp
+++ Base/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream> // iostream(input output stream) is just a file from standard library
 using namespace std;
 int main() {
+    int x;
+    //cin >> x;
+    cout << " " << " " << " " << " " << " " << " " << "/" << "\\" << "_" << "/" << "\\" << endl;
+    cout << "/" << "\\" << " " << " " << " " << "/" << " " << "o" << " " << "o" << " " << "\\" << endl;
 
+
+
+
+
+
+
+
+/*
+
+      /\_/\
+ /\  / o o \
+//\\ \~(*)~/
+`  \/   ^ /
+   | \|| ||
+   \ '|| ||
+    \)()-())
+*/
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
     cout << "Hello World\n"; 
 // will create an extra line since I put \n and endl at the same time
     cout << endl; // endl and \n helps to go to the next line but it should be cout << "\n";
@@ -10,5 +49,12 @@ int main() {
 // :: helps to sind the function
     
 
-return 0;
-}
+
+
+
+
+
+
+
+
+*/
