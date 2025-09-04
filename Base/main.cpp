8acca906a19e1089526fb79d1c2ec01aa34ab52b
Taha <ta753090@gmail.com>
2025-09-03 22:12:57 -0400

    File added

--- Base/main.cpp
+++ Base/main.cpp
@@ -1,72 +1,15 @@
 #include <iostream> // iostream(input output stream) is just a file from standard library
 using namespace std;
 int main() {
-    int x;
-    //cin >> x;
-    cout << " " << " " << " " << " " << " " << "/" << "\\" << "_" << "/" << "\\" << endl;
-    cout <<" " << "/" << "\\" << " " << "/" << " " << "o" << " " << "o" << " " << "\\" << endl;
-    cout << "/" << "/" << "\\" << "\\" << "\\" << "~" << "(" << "*" << ")" << "~" << "/" << endl;
-    //cout << "`" << " " << " " << " " << "\\" << "/" << " " << " " << " " << "^" << " " << "/" << endl;
-    //cout << " " << " " << " " << " " << "|" << " " << "\\" << "|" << "|" << " " << "|" << "|" << endl;
-    //cout << " " << " " << " " << " " << "\\" << " " << "'" << "|" << "|" << " " << "|" << "|" << endl;
-    //cout << " " << " " << " " << " " << " " << "\\" << ")" << "()" << "-" << "()" << ")" << endl;
- 
+    cout << "Hello World\n"; 
+    cout << "Hello World\n"; ///will create an extra line since I put \n and endl at the same time
+    cout << endl; // endl and \n helps to go to the next line but it should be cout << "\n";
+    cout << "Hello";
+//std = standard library, cout = cin >>acter out, and to terminate this statement make sure to use ;
+// :: helps to f
 
 
 
-
-
-
-
-/*
-      /\_/\
- /\  / o o \
-//\\ \~(*)~/
-`  \/   ^ /
-   | \|| ||
-   \ '|| ||
-    \)()-())
-
-
-
-
-
-
-
-
-*/
 return 0;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    cout << "Hello World\n"; 
-// will create an extra line since I put \n and endl at the same time
-    cout << endl; // endl and \n helps to go to the next line but it should be cout << "\n";
-    cout << "Hello";
-//std = standard library, cout = character out, and to terminate this statement make sure to use ;
-// :: helps to sind the function
-    
-
-
-
-
-
-
-
-
-
-*/
