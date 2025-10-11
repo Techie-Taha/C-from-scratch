7cb507692e77b0589828417621692f9df001c462
Taha <ta753090@gmail.com>
2025-10-11 18:54:45 -0400

    File added
    File added

---
+++ Loops/forloop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+
+
+
+// Initialization which happens only for once, i = 1;
+// condtion, i < 5; if true then print the result "I love you". In the terminal I love you
+// then i++ means increament the value by 1, check condition again, is 2 less than 5, yes. If true then print the result I love you again, if not then break the loop
+
+
+for(int i = 1; i < 5; i++){
+    cout << "I love you" << endl;
+}
+
+    return 0;
+}
---
+++ Loops/gg.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+
+int n;
+cin >> n;
+
+for(int i = 1; i <= 10; i++){
+    if(i % 2 == 0){
+        cout << i << endl; // before printing just put a condition. just print i
+    }
+}
+return 0;
+}
