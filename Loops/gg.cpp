8ead8cefabc4efe5645293d8a0fa17629017ebc3
Taha <ta753090@gmail.com>
2025-10-11 19:23:00 -0400

    File added

--- Loops/gg.cpp
+++ Loops/gg.cpp
@@ -3,14 +3,15 @@ using namespace std;
 int main(){
 
 
-
+int a = 3;
 int n;
 cin >> n;
 
-for(int i = 1; i <= 10; i++){
-    if(i % 2 == 0){
-        cout << i << endl; // before printing just put a condition. just print i
+for(int i = 4; i <= 3*n+1; i= i+3){
+    
+        cout << a << endl; // before printing just put a condition. just print i
+        a = a *3;
     }
-}
+
 return 0;
 }
