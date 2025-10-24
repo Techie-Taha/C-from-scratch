7a516dfaf6e00220073b448e0855b9f860887e71
Taha <ta753090@gmail.com>
2025-10-23 21:30:41 -0400

    File added

--- ZyBooks/main.cpp
+++ ZyBooks/main.cpp
@@ -1,18 +1,18 @@
-#include<cmath>
-#include<iomanip>
+//#include<cmath>
+//#include<iomanip>
 #include<iostream> 
 using namespace std;
 int main (){
 
+   double x;
 
-string num;
-cin >> num;
-cout << "(" << num.substr(0, 3) << ")" << num.substr(4,6) << " -" << num.substr(7,9) << endl;
-
-
-
-
-
+   cin >> x;
+   if(x<30.5 || x > 75.5){
+cout << "Skip" << endl;
+   }
+   else {
+    cout << "Authorize" << endl;
+   }
 
 
 
